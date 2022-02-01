355e935748e04a48bd9d952a87706275f953c56f
Malte <malteweiss93@gmail.com>
2022-02-01 23:07:04 +0100

    Adds circular array

---
+++ Assets/CircularArray.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CircularArray : ArrayModifier
+{
+    [SerializeField] private float radius;
+
+    protected override Vector3 RelativePositionFor(int index, Bounds bounds)
+    {
+        var alpha = ((Mathf.Deg2Rad * 360) / Amount) * index;
+
+        return PolarToCartesian(alpha);
+    }
+
+    private Vector3 PolarToCartesian(float alpha)
+    {
+        var x = radius * Mathf.Cos(alpha);
+        var z = radius * Mathf.Sin(alpha);
+
+        return new Vector3(x, 0f, z);
+    }
+}
